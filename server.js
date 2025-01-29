92f5f9372de133fc1603619a25ffac120f027fb0
Pratik Kumar <pratikk12093@gmail.com>
2025-01-29 22:17:31 +0530

    New version

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const db = require('./db');
 app.get('/', function (req, res) {
     res.send('How can i help you sir..')
 })
-
+//commit for next version
 app.listen(4000,()=>{
     console.log("Listening on port number 4000")
 })//server at address of localhost 4000
